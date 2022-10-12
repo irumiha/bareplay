c18988bb5cb19af9932471e86c7010cb6f8e8a59
Igor Rumiha <igorrumiha@gmail.com>
2022-10-12 23:40:49 +0200

    Remove irrelevant comment

--- src/main/scala/application/DatabaseExecutionContext.scala
+++ src/main/scala/application/DatabaseExecutionContext.scala
@@ -4,8 +4,6 @@ import play.api.libs.concurrent.CustomExecutionContext
 
 import scala.concurrent.ExecutionContext
 
-// Make sure to bind the new context class to this trait using one of the custom
-// binding techniques listed on the "Scala Dependency Injection" documentation page
 trait DatabaseExecutionContext extends ExecutionContext
 
 class DatabaseExecutionContextImpl(system: ActorSystem)
