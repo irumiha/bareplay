2ee11a98dbfc1bf113c347b545ce3f70dbb7c22e
Igor Rumiha <igorrumiha@gmail.com>
2022-11-24 23:39:50 +0100

    WIP Improve logging and scaladoc.

--- src/main/scala/application/security/RolesSecuredBlocks.scala
+++ src/main/scala/application/security/RolesSecuredBlocks.scala
@@ -6,7 +6,13 @@ import play.api.mvc.Security.AuthenticatedRequest
 
 import scala.concurrent.Future
 
-trait RolesSecuredBlocks {
+/**
+ * Both methods here wrap an existing action block by first checking for existence of a valid JWT in a cookie.
+ * If the JWT is valid then check the roles given in the "realm_access" \ "roles" claim.
+ *
+ * ALL REQUESTED ROLES MUST BE PRESENT IN JWT
+ */
+trait RolesSecuredBlocks extends play.api.Logging {
   type AuthenticatedBlock[A] = AuthenticatedRequest[A, Authentication] => Result
   def withRoles(
     roles: Set[String]
@@ -15,6 +21,7 @@ trait RolesSecuredBlocks {
     if (matchingRoles.equals(roles)) {
       block(request)
     } else {
+      logger.warn(s"Roles mismatch, received: ${request.user.roles}, required: ${roles}")
       Forbidden
     }
   }
@@ -27,6 +34,7 @@ trait RolesSecuredBlocks {
     if (matchingRoles.equals(roles)) {
       block(request)
     } else {
+      logger.warn(s"Roles mismatch, received: ${request.user.roles}, required: ${roles}")
       Future.successful(Forbidden)
     }
   }
--- src/main/scala/application/security/SecurityActionWrapper.scala
+++ src/main/scala/application/security/SecurityActionWrapper.scala
@@ -9,18 +9,27 @@ case class SecurityActionWrapper(
     userAuthBuilder: UserAuthenticatedBuilder
 ) extends play.api.Logging {
 
+  /**
+   * Wraps an existing action by checking for existence of a valid JWT in a cookie
+   */
   def apply[A](action: Action[A]): Action[A] =
     userAuthBuilder.async(action.parser) { request =>
       action(request)
     }
 
+  /**
+   * Wraps an existing action by checking for existence of a valid JWT in a cookie.
+   * If the JWT is valid then check the roles given in the "realm_access" \ "roles" claim.
+   *
+   * ALL REQUESTED ROLES MUST BE PRESENT IN JWT
+   */
   def apply[A](roles: Set[String])(action: Action[A]): Action[A] =
     userAuthBuilder.async(action.parser) { request =>
       val matchingRoles = request.user.roles intersect roles
       if (matchingRoles.equals(roles)) {
         action(request)
       } else {
-        logger.warn(s"Disallowing access for ${request.path}, roles mismatch")
+        logger.warn(s"Roles mismatch, received: ${request.user.roles}, required: ${roles}")
         Future.successful(Forbidden)
       }
     }
