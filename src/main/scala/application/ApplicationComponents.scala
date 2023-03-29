b356f73ca0127de27da31c001f626ef0c121faa3
Igor Rumiha <igor.rumiha@netlync.com>
2023-03-29 23:09:23 +0200

    Use reusable testcontainers in local dev runs.

--- src/main/scala/application/ApplicationComponents.scala
+++ src/main/scala/application/ApplicationComponents.scala
@@ -1,18 +1,25 @@
 package application
 
+import akka.Done
+import akka.actor.CoordinatedShutdown
 import application.security.Authentication
+
+import scala.concurrent.ExecutionContext.Implicits.global
 import com.softwaremill.tagging.{@@, Tagger}
+import devcontainers.AllAppContainers
+import org.testcontainers.containers.GenericContainer
 import play.api.ApplicationLoader.Context
 import play.api.cache.AsyncCacheApi
 import play.api.cache.caffeine.CaffeineCacheComponents
 import play.api.db.{DBComponents, Database, HikariCPComponents}
 import play.api.libs.ws.ahc.AhcWSComponents
-import play.api.{Application, ApplicationLoader, BuiltInComponentsFromContext, LoggerConfigurator}
+import play.api.{Application, ApplicationLoader, BuiltInComponentsFromContext, Configuration, LoggerConfigurator}
 
 import java.time.Clock
+import scala.concurrent.Future
 
 class ApplicationComponents(context: Context)
-    extends BuiltInComponentsFromContext(context)
+  extends BuiltInComponentsFromContext(context)
     with DBComponents
     with HikariCPComponents
     with play.filters.HttpFiltersComponents
@@ -38,4 +45,26 @@ class Loader extends ApplicationLoader {
   def load(context: Context): Application = new ApplicationComponents(
     context
   ).application
+}
+
+class DevLoader extends ApplicationLoader with AllAppContainers {
+
+  override def realmName: String = "dev-realm"
+
+  override def containers: Seq[GenericContainer[_]] =
+    Seq(postgresContainer.withReuse(true), keycloakContainer.withReuse(true))
+
+  override def load(context: Context): Application = {
+    containers.foreach(c => c.start())
+
+    val configurationFromContainers = Configuration.from(containerConfiguration)
+
+    val devContext = context.copy(initialConfiguration =
+      configurationFromContainers.withFallback(context.initialConfiguration)
+    )
+
+    new ApplicationComponents(
+      devContext
+    ).application
+  }
 }
